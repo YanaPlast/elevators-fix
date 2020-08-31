12f2f2e8e41a4f5a6c9f102c685cd66ca4c63cec
Yana <yana.vsu@gmail.com>
2020-08-31 14:49:37 +0300

    отложенная загрузка карты, картинки

--- elevators_fix/js/map.js
+++ elevators_fix/js/map.js
@@ -1,12 +1,1 @@
-/* Показывать карту только когда до нее докрутили */
-
-var reviews = $('.reviews');
-var reviewsTop = reviews.offset().top;
-$(window).bind('scroll', function() {
-	var windowTop = $(this).scrollTop();
-	if(windowTop > reviewsTop) {
-		console.log('Докрутили');
-		//$('.map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A8c6b30629cd5296d6be780fca53cd6b369d5d82b410f3c92df836b562f276e85&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=false"></script>')
-		$(window).unbind('scroll')
-	}
-})
+		$('#map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A8c6b30629cd5296d6be780fca53cd6b369d5d82b410f3c92df836b562f276e85&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=false"></script>');
